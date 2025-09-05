007e5e16919b154e8af086991f7743238534d8ed
mert-unsal <mertusal0@gmail.com>
2025-09-06 01:55:18 +0200

    feat: enhance FileProcessingException handling

--- src/main/java/com/ikea/warehouse_data_ingestion_service/exception/FileProcessingException.java
+++ src/main/java/com/ikea/warehouse_data_ingestion_service/exception/FileProcessingException.java
@@ -6,11 +6,29 @@ package com.ikea.warehouse_data_ingestion_service.exception;
  */
 public class FileProcessingException extends RuntimeException {
 
+    private final String error;
+
     public FileProcessingException(String message) {
         super(message);
+        this.error = null;
     }
 
     public FileProcessingException(String message, Throwable cause) {
         super(message, cause);
+        this.error = null;
+    }
+
+    public FileProcessingException(String message, String error) {
+        super(message);
+        this.error = error;
+    }
+
+    public FileProcessingException(String message, String error, Throwable cause) {
+        super(message, cause);
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
     }
 }
