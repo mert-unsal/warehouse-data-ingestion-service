c731638964394124516a8b8ab077ff47b48a639a
mert-unsal <mertusal0@gmail.com>
2025-09-05 11:24:04 +0200

    It initializes warehouse data ingestion service with Maven setup and basic application structure.

---
+++ src/main/java/com/ikea/warehouse_data_ingestion_service/WarehouseDataIngestionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ikea.warehouse_data_ingestion_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseDataIngestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseDataIngestionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ikea/warehouse_data_ingestion_service/WarehouseDataIngestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikea.warehouse_data_ingestion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseDataIngestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
