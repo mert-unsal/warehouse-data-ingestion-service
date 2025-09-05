c27322e85b507aa3c7cd66693d03d8a481e71fbe
mert-unsal <mertusal0@gmail.com>
2025-09-06 01:53:47 +0200

    feat: enhance KafkaProducerService functionality

--- src/main/java/com/ikea/warehouse_data_ingestion_service/service/KafkaProducerService.java
+++ src/main/java/com/ikea/warehouse_data_ingestion_service/service/KafkaProducerService.java
@@ -1,55 +1,52 @@
 package com.ikea.warehouse_data_ingestion_service.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
 
+@Slf4j
+@Profile("!test")
 @Service
+@RequiredArgsConstructor
 public class KafkaProducerService {
 
-    private static final Logger logger = LoggerFactory.getLogger(KafkaProducerService.class);
-
     private final KafkaTemplate<String, Object> kafkaTemplate;
-    private final String topic;
 
-    public KafkaProducerService(KafkaTemplate<String, Object> kafkaTemplate,
-                                @Value("${kafka.topic}") String topic) {
-        this.kafkaTemplate = kafkaTemplate;
-        this.topic = topic;
+    @Value("${kafka.topic.product}")
+    private String productTopic;
+
+    @Value("${kafka.topic.product}")
+    private String inventoryTopic;
+
+    public void sendProductData(Object productData) {
+        sendMessage(productTopic, "product", productData);
     }
 
-    public void sendMessage(Object message) {
-        sendMessage(null, message);
+    public void sendInventoryData(Object inventoryData) {
+        sendMessage(inventoryTopic, "inventory", inventoryData);
     }
 
-    public void sendMessage(String key, Object message) {
+    private void sendMessage(String topic, String key, Object message) {
         try {
             CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send(topic, key, message);
 
             future.whenComplete((result, ex) -> {
                 if (ex == null) {
-                    logger.info("Sent message=[{}] with key=[{}] to topic=[{}] with offset=[{}]",
+                    log.info("Sent message=[{}] with key=[{}] to topic=[{}] with offset=[{}]",
                             message.getClass().getSimpleName(), key, topic, result.getRecordMetadata().offset());
                 } else {
-                    logger.error("Unable to send message=[{}] with key=[{}] to topic=[{}] due to: {}",
+                    log.error("Unable to send message=[{}] with key=[{}] to topic=[{}] due to: {}",
                             message.getClass().getSimpleName(), key, topic, ex.getMessage());
                 }
             });
         } catch (Exception e) {
-            logger.error("Error sending message to Kafka: {}", e.getMessage(), e);
+            log.error("Error sending message to Kafka: {}", e.getMessage(), e);
         }
-    }
-
-    public void sendInventoryData(Object inventoryData) {
-        sendMessage("inventory", inventoryData);
-    }
-
-    public void sendProductData(Object productData) {
-        sendMessage("product", productData);
     }
 }
