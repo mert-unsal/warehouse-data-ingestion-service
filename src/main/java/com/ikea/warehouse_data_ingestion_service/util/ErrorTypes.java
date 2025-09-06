b9a962e3fc6059b80d0d84f41773c29a21b8b3a5
mert-unsal <mertusal0@gmail.com>
2025-09-06 13:44:00 +0200

    feat: add ErrorTypes utility class for centralized error type management

---
+++ src/main/java/com/ikea/warehouse_data_ingestion_service/util/ErrorTypes.java
@@ -0,0 +1,8 @@
+package com.ikea.warehouse_data_ingestion_service.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ErrorTypes {
+    public static final String FILE_PROCESSING_ERROR = "FILE_PROCESSING_ERROR";
+}
