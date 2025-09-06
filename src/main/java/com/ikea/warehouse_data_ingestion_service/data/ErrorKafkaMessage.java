9ac546f866a9508ad77837c8f1a1542a69d64a0c
mert-unsal <mertusal0@gmail.com>
2025-09-06 13:43:56 +0200

    chore: remove obsolete ErrorKafkaMessage class after refactor

--- src/main/java/com/ikea/warehouse_data_ingestion_service/data/ErrorKafkaMessage.java
+++
@@ -1,9 +0,0 @@
-package com.ikea.warehouse_data_ingestion_service.data;
-
-public record ErrorKafkaMessage(
-    String key,
-    Object originalMessage,
-    String originalTopic,
-    String errorMessage,
-    long timestamp
-) {}
